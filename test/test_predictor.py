74f9c759b3008b1bd8a6ac8def7683718a6e8e6b
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2017-03-24 11:41:58 -0400

    import more of numpy

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -1,5 +1,6 @@
 from pepnet import Predictor, SequenceInput, NumericInput, Output
-import numpy as np
+from numpy.random import randn
+from numpy import log, exp
 from nose.tools import eq_
 
 
@@ -9,7 +10,7 @@ def test_simple_numeric_predictor():
         outputs=[Output(dim=1, name="y", activation="sigmoid")],
         hidden_layer_sizes=[30],
         hidden_activation="relu")
-    y = predictor.predict({"x": np.random.randn(10, 30)})["y"]
+    y = predictor.predict({"x": randn(10, 30)})["y"]
     eq_(len(y), 10)
 
 
@@ -33,7 +34,7 @@ def test_two_input_predictor():
         hidden_activation="relu")
     y = predictor.predict({
         "x1": ["SFY-"] * 10,
-        "x2": np.random.randn(10, 30)})["y"]
+        "x2": randn(10, 30)})["y"]
     eq_(len(y), 10)
 
 def test_predictor_output_transform():
@@ -44,12 +45,12 @@ def test_predictor_output_transform():
                 name="y",
                 dim=1,
                 activation="sigmoid",
-                transform=np.log,
-                inverse_transform=np.exp)],
+                transform=log,
+                inverse_transform=exp)],
         hidden_layer_sizes=[30],
         hidden_activation="relu")
-    y = predictor.predict({"x": np.random.randn(10, 30)})["y"]
+    y = predictor.predict({"x": randn(10, 30)})["y"]
     eq_(len(y), 10)
     # make sure transformed outputs are within given bounds
-    assert np.exp(0.0) <= y.min() <= np.exp(1.0)
-    assert np.exp(0.0) <= y.max() <= np.exp(1.0)
+    assert exp(0.0) <= y.min() <= exp(1.0)
+    assert exp(0.0) <= y.max() <= exp(1.0)
