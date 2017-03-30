388aa49e29a5bc7c4b366cf2bd1fdbe5f7cabeb0
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2017-03-30 19:26:27 -0400

    tests without names

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -6,6 +6,15 @@ from nose.tools import eq_
 
 def test_simple_numeric_predictor():
     predictor = Predictor(
+        inputs=[NumericInput(dim=30)],
+        outputs=[Output(dim=1, activation="sigmoid")],
+        hidden_layer_sizes=[30],
+        hidden_activation="relu")
+    y = predictor.predict(randn(10, 30))
+    eq_(len(y), 10)
+
+def test_simple_numeric_predictor_named():
+    predictor = Predictor(
         inputs=[NumericInput(name="x", dim=30)],
         outputs=[Output(dim=1, name="y", activation="sigmoid")],
         hidden_layer_sizes=[30],
@@ -16,13 +25,21 @@ def test_simple_numeric_predictor():
 
 def test_simple_sequence_predictor():
     predictor = Predictor(
+        inputs=[SequenceInput(length=4, variable_length=True)],
+        outputs=[Output(dim=1, activation="sigmoid")],
+        hidden_layer_sizes=[30],
+        hidden_activation="relu")
+    y = predictor.predict(["SFY-"] * 10)
+    eq_(len(y), 10)
+
+def test_simple_sequence_predictor_named():
+    predictor = Predictor(
         inputs=[SequenceInput(length=4, name="x", variable_length=True)],
         outputs=[Output(dim=1, activation="sigmoid", name="y")],
         hidden_layer_sizes=[30],
         hidden_activation="relu")
     y = predictor.predict({"x": ["SFY-"] * 10})["y"]
     eq_(len(y), 10)
-
 
 def test_two_input_predictor():
     predictor = Predictor(
